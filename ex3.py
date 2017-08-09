1da37ed6b20850100b43cf05585bd48d163d2a31
Anton Bauer <anton.bauer@keenswh.com>
2017-08-09 13:23:14 +0200

    Changed the calculations to output floating point numbers

--- ex3.py
+++ ex3.py
@@ -11,27 +11,28 @@
 # ................... <= (less-than-equal)
 # ................... >= (greater-than-equal)
 
-
+# print the following counting of the chickens
 print "I will now count my chickens:"
 
-print "Hens", 25 + 30 / 6
-print "Roosters", 100 - 25 * 3 % 4
+# Count the Hens and Roosters
+print "Hens", 25 + 30 / 6.0
+print "Roosters", 100 - 25 * 3 % 4.0
 
+# Calculate the amount of eggs
 print "Now I will count the eggs:"
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4.0 + 6
 
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
-
+# Is 3 + 2 < 5 - 7 true or not?
 print "Is it true that 3 + 2 < 5 - 7?"
-
 print 3 + 2 < 5 - 7
 
+# Explain why 3 + 2 IS NOT less-than 5 - 7
 print "What is 3 + 2?", 3 + 2
 print "What is 5 - 7?", 5 - 7
-
 print "Oh, that's why it's false."
 
+# More comparisons
 print "How about some more."
-
-print "Is it greater?", 5 > -2, "1 + 1"
+print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= -2
 print "Is it less or equal?", 5 <= -2
