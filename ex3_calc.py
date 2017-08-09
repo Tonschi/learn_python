839e18658c44d0d963d841eafd6cf2bddeade44a
Anton Bauer <anton.bauer@keenswh.com>
2017-08-09 15:33:25 +0200

    Added exrecise calculation test and exercise 4 variables introduction script

---
+++ ex3_calc.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+print "1920 x 1080 Aspect ratio:", 1920/1080.0
+print "1920 x 1200 Aspect ratio:", 1920/1200.0
+print "1280 x 720 Aspect ratio:", 1280/720.0
+
+print "Does 1920x1200 have a bigger aspect ratio than 1920x1080?"
+print 1920 / 1080 < 1920 / 1200
