7bfaec2c8f25e0297440171a7798108c1747962b
Tonschi <timokino123-mail@yahoo.com>
2017-08-09 08:14:33 +0200

    first commit

---
+++ ex1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not töuch this.'
+print "All hail the 'print' command!"
---
+++ ex2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# This is a comment. Anything after the # character is ignored by python
+
+print "This is a text output." # This is a comment.
+
+# Temporary commented out section of the code
+# print "Do something useful!"
+
+print "This is something more useful."
