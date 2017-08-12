708b5b61c25256109375ac2a34a1491bb9533edc
Tonschi <tonschi@tonschi.com>
2017-08-12 23:51:09 +0200

    exercise 6

--- ex6.py
+++ ex6.py
@@ -1,20 +1,30 @@
+# start of the binary joke
 x = "There are %d types or people." % 10
+
+# declare binary variable
 binary = "binary"
+
+# declar do_not variable
 do_not = "don't"
+
+# end of binary joke
 y = "Those who know %s and those who %s." % (binary, do_not)
 
+# print the binary joke
 print x
 print y
 
 print "I said: %r." % x
 print "I also said: '%s'." % y
 
-hilarious = False
+hilarious = True
 joke_evaluation = "Isn't that joke so funny?! %r"
 
 print joke_evaluation % hilarious
 
+# store strings to variables
 w = "This is the left side of..."
 e = "a string with a right side."
 
+# print the combined string
 print w+e
