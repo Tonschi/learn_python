413fd54efbd258798487c654d3e84bb04543b8e0
Tonschi <tonschi@tonschi.com>
2017-08-17 08:56:14 +0200

    exercises 12 & 13. All about processing user input from the console

---
+++ ex12.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# get the user input and store it in variables
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+# just print the variables
+print "So, you're %r years old, %r tall and %r heavy." % (age, height, weight)
